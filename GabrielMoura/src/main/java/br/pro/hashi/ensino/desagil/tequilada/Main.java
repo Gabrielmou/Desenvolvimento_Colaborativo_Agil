00d6ffe13d513d7e30d6470062d84cae79c59547
Gabrielmou <gabrielmoura98@gmail.com>
2018-02-28 18:26:39 -0300

    incremento do projeto 2

--- GabrielMoura/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
+++ GabrielMoura/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
@@ -1,8 +1,42 @@
 package br.pro.hashi.ensino.desagil.tequilada;
 
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Projeto 1");
-	}
+		BufferedReader leitor = null;
+		
+		try{
+	         File arquivo = new File("C:/Users/Gabriel Moura/workspace/GabrielMoura/src/main/java/br/pro/hashi/ensino/desagil/tequilada/incre2.txt");
+	         leitor = new BufferedReader(new FileReader(arquivo));
+	         
+	         String linha;
+		     linha = leitor.readLine();
+		     
+		     while ((linha = leitor.readLine()) != null) {
+		        StringBuilder novalinha = new StringBuilder(linha);
+		        
+		        for (int i = 0; i < novalinha.length(); i++) {
+		        	if (novalinha.charAt(i) == '#') {
+		        		novalinha.setCharAt(i, 'X');
+		        	}		
+		        }
+		        System.out.println(novalinha);
+		     }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	         leitor.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	  }
 
 }
