d5d38540f0e0771bf1b98cbb7c2656b3941d0ffc
Gabrielmou <gabrielmoura98@gmail.com>
2018-03-12 10:29:30 -0300

    Guia 5

---
+++ Guia5/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
@@ -0,0 +1,34 @@
+package br.pro.hashi.ensino.desagil.tequilada;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class Main {
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	Model model = new Model();
+
+            	View view = new View(model);
+
+            	Controller controller = new Controller(model, view);
+            	
+            	JFrame frame = new JFrame();
+
+            	frame.setContentPane(view);
+
+            	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            	frame.setResizable(false);
+
+            	frame.pack();
+
+            	frame.setVisible(true);
+
+            	Timer timer = new Timer(1000, controller);
+
+            	timer.start();
+            }
+        });
+	}
+}
